afacab73d4fb76fc9cc11f920b00fb68ee5e0643
jetsontwo <jetsontwo@gmail.com>
2017-10-28 15:41:24 -0700

    Made Spawning Infinite and Music Loop
    Just stuff I forgot to do.

--- Assets/Scripts/Game_Manager.cs
+++ Assets/Scripts/Game_Manager.cs
@@ -31,9 +31,13 @@ public class Game_Manager : MonoBehaviour {
 
     private IEnumerator spawn()
     {
-        GameObject g = Instantiate(enemies[Random.Range(0, 3)], spawn_locations[Random.Range(0, 4)].transform.position, Quaternion.identity, enemy_holder.transform);
-        g.GetComponent<SpriteRenderer>().color = new Color(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f));
-        yield return new WaitForSeconds(Random.Range(0.6f, 1f));
+        while(true)
+        {
+            GameObject g = Instantiate(enemies[Random.Range(0, 3)], spawn_locations[Random.Range(0, 4)].transform.position, Quaternion.identity, enemy_holder.transform);
+            g.GetComponent<SpriteRenderer>().color = new Color(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f));
+            yield return new WaitForSeconds(Random.Range(0.6f, 1.2f));
+        }
+
     }
 
 }
