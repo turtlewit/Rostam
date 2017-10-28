aa10d08dad75668fceda8582dbc970dcc726edbf
jetsontwo <jetsontwo@gmail.com>
2017-10-27 20:24:56 -0700

    Created Basic Movement and Jump Script
    Allows the player to move left and right and jump. Also added tags for
    platforms and for enemies

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -4,13 +4,46 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour {
 
+    private Rigidbody2D rb;
+    public float speed, jump_power;
+    private bool grounded = false;
+
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        touching_ground();
+		if(Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-1 * Time.deltaTime * speed, rb.velocity.y);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(1 * Time.deltaTime * speed, rb.velocity.y);
+        }
+        if(Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            rb.AddForce(new Vector2(0, jump_power), ForceMode2D.Impulse);
+            grounded = false;
+        }
+
+        if(!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
 	}
+
+
+    void touching_ground()
+    {
+        RaycastHit2D rh = Physics2D.Raycast(transform.position, Vector2.down, .55f);
+        if(rh)
+        {
+            if (rh.collider.tag == "Platform")
+                grounded = true;
+        }
+    }
 }
