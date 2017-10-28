d0aeb24033adb86fa92d3ef1855cd80c61b4e38a
jetsontwo <jetsontwo@gmail.com>
2017-10-28 14:25:01 -0700

    Added in Enemy Art and Player Arm Controls
    Enemies and player now have art and the player's arm and particle
    systems now track the mouse and gun.

---
+++ Assets/Scripts/Player/Player_Arm_Controller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Arm_Controller : MonoBehaviour {
+
+    private Vector2 world_pos, target, pos;
+    public float offset;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        world_pos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+        target = new Vector2(world_pos.x, world_pos.y);
+        pos = new Vector2(transform.position.x, transform.position.y);
+        Vector2 dir = (target - pos).normalized;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, (Mathf.Rad2Deg * Mathf.Atan2(dir.y, dir.x)) + offset));
+
+    }
+}
