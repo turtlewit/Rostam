38b0b740b04337c9cd27664a41d810a791cabd55
jetsontwo <jetsontwo@gmail.com>
2017-10-29 12:48:09 -0700

    Added Highlights to the Continue Scene
    Now the text highlights as you move your mouse over it.

---
+++ Assets/Scripts/Highlight_Script.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Highlight_Script : MonoBehaviour {
+
+    public Text t;
+
+
+	void OnMouseEnter()
+    {
+        t.color = new Color(1, 1, 0);
+    }
+
+    void OnMouseExit()
+    {
+        t.color = new Color(1, 1, 1);
+    }
+}
