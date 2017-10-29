3eea1a822b714488e7cd2793abdbfb6ffca82ecf
jetsontwo <jetsontwo@gmail.com>
2017-10-28 22:30:57 -0700

    Finished Continue Scene
    Continue scene now lets you quit or restart the game. Also fixed some
    bugs so the cursor gets reset in the continue scene.

---
+++ Assets/Scripts/Continue_Scene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Continue_Scene : MonoBehaviour
+{
+
+    public void restart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void close()
+    {
+        Application.Quit();
+    }
+}
