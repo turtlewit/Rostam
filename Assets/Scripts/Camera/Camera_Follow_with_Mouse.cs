dd976194b98ce627622fc9ad755e7d6e1f5aa671
jetsontwo <jetsontwo@gmail.com>
2017-10-29 15:49:58 -0700

    Added Camera Movement when Player Move Mouse
    Camera now follows between mouse and player so player can look ahead and
    see where they are going with the mouse.

--- Assets/Scripts/Camera/Camera_Follow_with_Mouse.cs
+++ Assets/Scripts/Camera/Camera_Follow_with_Mouse.cs
@@ -18,11 +18,10 @@ public class Camera_Follow_with_Mouse : MonoBehaviour {
         Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         position_to_track = thing_to_track.transform.position;
-        position_to_track -= mouse;
-        position_to_track = new Vector3(position_to_track.x, position_to_track.y, -10);
 
-
-        transform.position = Vector2.Lerp(transform.position, position_to_track, track_speed * Time.deltaTime);
+        transform.position = Vector2.Lerp(transform.position, Vector2.Lerp(position_to_track, mouse, 0.3f), track_speed * Time.deltaTime);
         transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+
+            
     }
 }
