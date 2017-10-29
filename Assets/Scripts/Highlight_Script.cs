72f8d1431dbe5fdef382120bc1e2d1593f9ab489
jetsontwo <jetsontwo@gmail.com>
2017-10-29 12:51:26 -0700

    Added Game Over Music to Final Scene and Select Sounds
    Last scene now has music and blip sounds when you mouse over the
    buttons.

--- Assets/Scripts/Highlight_Script.cs
+++ Assets/Scripts/Highlight_Script.cs
@@ -6,11 +6,18 @@ using UnityEngine.UI;
 public class Highlight_Script : MonoBehaviour {
 
     public Text t;
+    private AudioSource a;
+
+    void Start()
+    {
+        a = GetComponent<AudioSource>();
+    }
 
 
 	void OnMouseEnter()
     {
         t.color = new Color(1, 1, 0);
+        a.Play();
     }
 
     void OnMouseExit()
